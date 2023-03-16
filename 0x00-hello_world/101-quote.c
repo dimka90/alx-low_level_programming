cc4265a0b66b8cffcaf9786d3050c72b5094407d
dimka90 <dimkayilrit@gmail.com>
2023-03-16 20:36:22 +0000

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is\" - Dora Korpar, 2015-10-19\n", 58);
+	write(STDOUT_FILENO, "and that piece of art is\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
