16fadc109af4bcb665e30ec47d3640e1826a55cc
dimka90 <dimkayilrit@gmail.com>
2023-03-16 20:35:13 +0000

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
