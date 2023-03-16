76441c331787adeaa2663c7d4af8b92a7d5f9d09
dimka90 <dimkayilrit@gmail.com>
2023-03-16 13:46:55 +0000

    /**  * my_function - This is a description  */

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - Entry point for the program
- *return - 0 for success and anyother value for error
+ * return - 0 for success and anyother value for error
  */
 int main(void)
 {
