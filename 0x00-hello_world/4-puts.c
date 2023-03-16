d086f180ce4806c5326d7785881028dd17584823
dimka90 <dimkayilrit@gmail.com>
2023-03-16 13:38:38 +0000

    printing to the output using c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-int main(){
+/**
+ * main - This is a description
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
