c49f44deb375473db8226f2b8e1d2743704e1b46
dimka90 <dimkayilrit@gmail.com>
2023-03-17 16:24:03 +0000

    printig numbers and hexidecimal content

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int number;
+
+int letter;
+
+for (number = 0; number <= 9; number++)
+{
+putchar(number);
+}
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+
+}
+return (0);
+}
