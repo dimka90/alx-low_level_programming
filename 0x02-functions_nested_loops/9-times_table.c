fdea2a7078db3bf9346386bbb97c7c2d5a39cd79
dimka90 <dimkayilrit@gmail.com>
2023-03-21 18:52:07 +0000

    Times table for 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include  "main.h"
+/**
+* times_table - times table4 for 9
+*
+*/
+
+void times_table(void)
+{
+int i;
+
+int j;
+
+for (i = 0; i < 9; i++)
+{
+
+for (j = 1; j < 9; j++)
+{
+_putchar(i * j) + '0');
+}
+}
+
+
+}
