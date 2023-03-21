86cd9b34d0d326a7fd6fdf12e76cd23e9675d3d8
dimka90 <dimkayilrit@gmail.com>
2023-03-21 15:57:37 +0000

    using nested for loops to print the lowercase letters 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print lowercase letters from a to z 10 times
+*
+* Description: print lowercase letters from a to z using a nested for loop
+*/
+void print_alphabet_x10(void)
+{
+int letter;
+
+int 1;
+
+for (i = 0; i <= 10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
+}
