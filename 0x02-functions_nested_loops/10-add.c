e10456e075171e2773686455f4cb05dbaf19e379
dimka90 <dimkayilrit@gmail.com>
2023-03-21 19:14:42 +0000

    A function that print the sum of two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,6 +3,8 @@
 *add - A functiont that adds two number and return the value
 *@firstnumber:  stored firstnumber
 *@secondnumber: stored secondnumber
+*
+* Return: Return the sum of firstnumber and secondnumber
 */
 int add(int firstnumber, int secondnumber)
 {
