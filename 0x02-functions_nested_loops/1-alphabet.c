6fc0c4a8fee89dc805176654e0aca12b56667756
dimka90 <dimkayilrit@gmail.com>
2023-03-21 15:45:35 +0000

    A function called (print_alphabet) that print lowercase letters from a to z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
-* print_alphabet : print lowercase letters from a to z
+* print_alphabet - print lowercase letters from a to z
+*
+* Description: print lowercase letters from a to z using a for loop
 */
 void print_alphabet(void)
 {
