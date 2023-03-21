790b73aedc78e77e14475a933f14823bd6e8e9d6
dimka90 <dimkayilrit@gmail.com>
2023-03-21 17:24:23 +0000

    Added a function that checks if a letter is an alphabet or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _isalpha - checks to alpahbet
+* @c: Stored interger value
+*
+* Description: The function check for alphabet
+*
+* Return: 1 for alphabet and 0 for non alpahabet
+*/
+int _isalpha(int c)
+{
+
+if (c >=60 || c <= 122)
+{
+
+return (1);
+}
+
+return (0);
+
+}
+
