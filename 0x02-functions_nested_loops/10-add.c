a002cd02c49a356d8b0d1273bcecdbda8e5f791f
dimka90 <dimkayilrit@gmail.com>
2023-03-21 19:12:21 +0000

    A function that add and returns two values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add - A functiont that adds two number and return the value
+*@firstnumber:  stored firstnumber
+*@secondnumber: stored secondnumber
+*/
+int add(int firstnumber, int secondnumber)
+{
+
+return (firstnumber + secondnumber);
+}
