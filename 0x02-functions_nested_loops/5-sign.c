51ea023e4888126f03e66069cb09e3ab50feebfe
dimka90 <dimkayilrit@gmail.com>
2023-03-21 17:40:44 +0000

    checks if a number is positve, zero or negetive

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - checks  for sign of an number
+* @c: Stored interger value
+*
+* Description: The function check for for 0,1 or -1
+*
+* Return: 1 for positive , 0 for 0 and -1 for negetive numbers
+*/
+int print_sign(int n);
+{
+
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+_putchar('-');
+return (-1);
+}
