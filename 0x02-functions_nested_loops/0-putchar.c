9a713a3792de9a82bef259104531745447d25dd6
dimka90 <dimkayilrit@gmail.com>
2023-03-21 14:25:01 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,14 @@
 #include <unistd.h>
-int _putchar(char *character)
+/**
+* _putchar - A function that print content a single character to the scree
+*
+* Return: return a result of the function
+*
+* @character:a char variable that serves as a parameter to the function
+*/
+int _putchar(char character)
 {
 
 
-return (write(1,&character,1));
+return (write(1, &character, 1));
 }
