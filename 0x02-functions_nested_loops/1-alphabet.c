52c0750a21f90ad3e0b06935d5e96dc5b61d108d
dimka90 <dimkayilrit@gmail.com>
2023-03-21 15:38:34 +0000

    printing lowercase letter on the screen

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
+/**
+* print_alphabet : print lowercase letters from a to z
+*/
 void print_alphabet(void)
 {
 int letter;
 
-
-for(letter='a'; letter<='z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 {
-
 _putchar(letter);
 }
 _putchar('\n');
