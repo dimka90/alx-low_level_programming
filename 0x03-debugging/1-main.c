12de5c11ae7d82aef2f707b0582c61814fa3484a
dimka90 <dimkayilrit@gmail.com>
2023-03-22 16:15:02 +0000

    Create comments that stop the infinite loop from running

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,11 +2,7 @@
 
 /**
 * main - causes an infinite loop
-* Description - This program cause the program to runan infinite loop
 * Return: 0
-*
-* while - loop
-* Description - while loop
 */
 
 int main(void)
@@ -17,15 +13,12 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-
-
 /*
-* while (i < 10)
-* {
-* putchar(i);
-* }
+*while (i < 10)
+*{
+*putchar(i);
+*}
 */
-printf("Infinite loop avoided! \\o/\n");
-
-return (0);
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
