2d0b8a881ed94933115a66782d10a139091191cb
dimka90 <dimkayilrit@gmail.com>
2023-03-23 13:29:36 +0000

    A function that checks for an integer number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+* _isdigit - checks for a digit
+*@c: a variable that stored an interger number
+*
+* Return: 1 for interger and 0 for others
+*/
+int _isdigit(int c)
+{
+
+if (c >= 48 && c <= 57)
+{
+
+return (1);
+
+}
+
+return (0);
+}
