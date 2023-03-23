0df223d5dfe8dd8d9b43eca9506328c45d51efbd
dimka90 <dimkayilrit@gmail.com>
2023-03-23 13:57:34 +0000

    Added  a function that prints the numbers, from 0 to 9, Except 2 and 4.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_most_numbers -  prints numbers, from 0 to 9, execpt 2 and 4
+* followed by a new line
+*
+*/
+void print_most_numbers(void)
+{
+int number;
+
+for (number = 0; number <= 9; number++)
+{
+if (number == 2 || 4)
+{
+continue;
+}
+_putchar('0' + number);
+}
+
+_putchar('\n');
+
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,5 @@ int _isdigit(int c);
 int mul(int a, int b);
 int _putchar(char character);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif
