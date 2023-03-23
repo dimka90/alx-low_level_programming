1ff4eb3018cb7c716f53e69e5d27fc08299cfb5a
dimka90 <dimkayilrit@gmail.com>
2023-03-23 13:34:37 +0000

    Added a function that multiply two number and return the result

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+*mul - Multiply two numbers
+*@a: stored value for the first number
+*@b: stored value for the second number
+*
+* Return: result of the two numbers multiply
+*/
+int mul(int a, int b)
+{
+
+return (a * b);
+
+}
