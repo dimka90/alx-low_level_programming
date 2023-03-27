a46c0d7208ee06c0032e450fa880f1303bc94eab
dimka90 <dimkayilrit@gmail.com>
2023-03-27 14:22:51 +0000

    A customize function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+* _strlen - A function that counts the number of words in a string
+*@s: stored the address of the string to count
+*
+* Return: Always 0 (Success)
+*/
+int _strlen(char *s)
+{
+int word_count;
+
+for (word_count = 0; s[word_count] != '\0'; word_count++);
+
+
+return (word_count);
+}
