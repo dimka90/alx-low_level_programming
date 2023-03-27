e3d0da2b57790af6c25bd9a3e0f2835d4389d3d6
dimka90 <dimkayilrit@gmail.com>
2023-03-27 13:51:22 +0000

    Added a function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+*swap_int - swapping of variable using pointer
+*@a: stored the address of the first pointer variable
+*@b: stored the address of the second pointer variable
+*/
+void swap_int(int *a, int *b)
+{
+
+int temp;
+
+temp = *a;
+
+*b = temp;
+
+*a = *b;
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
