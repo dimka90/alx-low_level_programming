d9077b9373b319509ac194a45ade2be7b1bf6bf4
dimka90 <dimkayilrit@gmail.com>
2023-03-29 20:05:50 +0000

    Added a function that converts lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+**string_toupper - convert lower letters to string
+*@s: Stored a pointer reference to a string
+* Return: A pointer for the converted string
+*/
+char *string_toupper(char *s)
+{
+
+int counter;
+
+for (counter = 0; s[counter] != '0'; counter++)
+{
+if (s[counter] >= 'a' && s[counter] <= 'z')
+{
+s[counter] = s[counter] - 32;
+}
+
+}
+return (s);
+}
