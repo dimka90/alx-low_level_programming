63c56b01541ed60307714929b5646fab031ad652
dimka90 <dimkayilrit@gmail.com>
2023-03-29 18:47:06 +0000

    fix some bug

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 int counter;
 
-for (counter = 0; s1[counter] != '\0' || s2[counter] != '\0'; counter++)
+for (counter = 0; s1[counter] != '\0' && s2[counter] != '\0'; counter++)
 {
 
 if (s1[counter] != s2[counter])
 {
-printf("%c %c", s1[counter], s2[counter]);
 if (s1[counter] > s2[counter])
 {
 
