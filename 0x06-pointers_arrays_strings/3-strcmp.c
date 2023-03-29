dd792b768b1fb8fffaa5deefbc5e8f6cf4c55961
dimka90 <dimkayilrit@gmail.com>
2023-03-29 18:18:17 +0000

    Corrected betty syntax error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 /**
+*_strcmp - compares two string together
+*@s1: A pointer varibale for the first string
+*@s2: A pointer variable for the second string
 *
+* Return: 0 for same string , positve or negative
 */
 int _strcmp(char *s1, char *s2)
 {
@@ -12,8 +16,8 @@ for (counter = 0; s1[counter] != '\0' || s2[counter] != '\0'; counter++)
 
 if (s1[counter] != s2[counter])
 {
-printf("%c %c",s1[counter],s2[counter]);
-if(s1[counter] > s2[counter])
+printf("%c %c", s1[counter], s2[counter]);
+if (s1[counter] > s2[counter])
 {
 
 return (s1[counter]);
