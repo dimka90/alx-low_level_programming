43a2397ab4d0e2991298ed54fc41a84151fa1780
dimka90 <dimkayilrit@gmail.com>
2023-03-29 18:53:53 +0000

    fix some syntax errors

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,7 +16,7 @@ for (counter = 0; s1[counter] != '\0' && s2[counter] != '\0'; counter++)
 
 if (s1[counter] != s2[counter])
 {
-return (s1[counter])-s2[counter]);
+return (s1[counter]-s2[counter]);
 }
 
 
