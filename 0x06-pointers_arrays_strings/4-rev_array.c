80ce06d0496f2a15519a077164640e2c345a5bcd
dimka90 <dimkayilrit@gmail.com>
2023-03-29 19:03:36 +0000

    fix betty syntax error

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,7 @@
 /**
-*
+*reverse_array - Reserve an array
+*@a: stored a pointer variable for the array
+*@n:stored the length of array
 */
 void reverse_array(int *a, int n)
 {
@@ -7,7 +9,7 @@ int counter;
 
 for (counter = 0; counter < n; counter++)
 {
-a[counter]=a[n];
+a[counter] = a[n];
 }
 
 }
