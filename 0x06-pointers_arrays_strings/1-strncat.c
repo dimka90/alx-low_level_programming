c83ed346eaa28d023372176ba031b83e41c27533
dimka90 <dimkayilrit@gmail.com>
2023-03-29 12:31:00 +0000

    Added a _strncat function that copy a string by a specific number if character

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <string.h>
+/**
+**_strcat- A function that append two strings
+*@dest: it stored a pointer to the destination string
+*@src: it store a pointer to the source string
+@n: stores the number of byte to append
+*Return: A char pointer to the destination string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int counter;
+
+int des_len;
+
+des_len = strlen(dest);
+
+
+
+for (counter = 0; counter <n; counter++)
+{
+
+dest[des_len + counter] = src[counter];
+
+}
+
+return (dest);
+
+}
