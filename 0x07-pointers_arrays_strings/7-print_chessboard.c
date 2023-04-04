0c9ef8890661e8f80eb64d62a59c51a1e533bbc6
dimka90 <dimkayilrit@gmail.com>
2023-04-04 02:49:22 +0000

    change variable name from i to row

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 	int row;
 	int column;
 
-	for (row = 0; row < 8; i++)
+	for (row = 0; row < 8; row++)
 	{
 		for (column = 0; column < 8; column++)
 			_putchar(a[row][column]);
