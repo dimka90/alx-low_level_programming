ece3b96b291f475c855a912bbc948fa64d243ca2
dimka90 <dimkayilrit@gmail.com>
2023-04-04 01:31:39 +0000

    Added a function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+*_strchr - a function that locates a character in a string.
+*@s: a pointer variable that contains string to search for
+*@c: a pointer varable for search for in string
+*
+*Return:NULL for non occurance and a pointer variable for a match
+*/
+char *_strchr(char *s, char c)
+{
+
+int counter = 0;
+
+while (*s != '\0')
+{
+if (*s == c)
+return (c)
+
+s++;
+
+}
+return (NULL);
+}
