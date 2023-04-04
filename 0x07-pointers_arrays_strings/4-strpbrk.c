be9b5fa48eaf30cf933a94966af54b532dd15f65
dimka90 <dimkayilrit@gmail.com>
2023-04-04 02:32:44 +0000

    corrected some login error

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,7 +10,7 @@ char *_strpbrk(char *s, char *accept)
 
 int counter_2;
 
-		for (counter = 0; s[counter] != '\0'; counter++)
+		for (counter = 0; s[counter] >= '\0'; counter++)
 		{
 			for (counter_2 = 0; accept[counter_2]; counter_2++)
 			{
