0d1a7f1f531dac2b31fa20440173abf85e01cdf6
dimka90 <dimkayilrit@gmail.com>
2023-04-04 01:02:39 +0000

    Added a function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+/**
+*_memcpy - A function that copies memory area
+*@dest: a pointer to the destination variable
+*@src: a pointer variable to copy from
+*@n: number of bytes to copy
+*
+*Return: A pointer to the the destination string
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int counter = 0;
+
+while (counter < n)
+{
+buffer[counter] = src[n];
+counter++;
+n--;
+}
+
+return (s);
+}
