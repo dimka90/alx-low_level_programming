8a21728b9c2c1afe24c5a8d81e68eb920d15cdfd
dimka90 <dimkayilrit@gmail.com>
2023-04-04 02:10:13 +0000

    fix some bug

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,20 +7,24 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int length = 0;
-	int counter_2;
-for(counter = 0; s[counter] !='\0'; counter ++)
+
+int counter;
+
+int counter_2;
+
+for (counter = 0; s[counter] != '\0'; counter++)
 	{
-		for (counter_2 = 0; accept[counter_2]; counter_2++)
-		{
-			if (s[counter] == accept[counter_2])
-			{
-				length++;
-				break;
-			}
-			else if (accept[counter_2 + 1] == '\0')
-				return (length);
-		}
-	}
-	return (length);
+for (counter_2 = 0; accept[counter_2]; counter_2++)
+{
+if (s[counter] == accept[counter_2])
+{
+length++;
+break;
+}
+else if (accept[counter_2 + 1] == '\0')
+return (length);
+}
+}
+return (length);
 }
 
