dcfd510b88fd8254c23c52607188d1c67c5094c7
dimka90 <dimkayilrit@gmail.com>
2023-04-05 21:44:06 +0000

    fix betty syntax error

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,9 @@
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
- *
- * Return: the resulting square root
+ *sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @number: number to calculate the sqaure root of
+ * @counter: counter variable
+ * Return: -1 for negative numbers and a recursive call
  */
 int sqrt_recursion(int number, int counter)
 {
@@ -13,6 +14,12 @@ int sqrt_recursion(int number, int counter)
 	return (sqrt_recursion(number, counter + 1));
 }
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+*/
 int _sqrt_recursion(int n)
 {
 if (n < 0)
@@ -24,10 +31,3 @@ else
 return (sqrt_recursion(n, 0));
 }
 }
-/**
- *sqrt_recursion - recurses to find the natural
- * square root of a number
- * @number: number to calculate the sqaure root of
- * @counter: counter variable
- * Return: -1 for negative numbers and a recursive call
- */
