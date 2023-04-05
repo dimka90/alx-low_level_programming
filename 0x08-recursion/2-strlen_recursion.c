3b409dd7600f03496790b06c77c48fce187b35d3
dimka90 <dimkayilrit@gmail.com>
2023-04-05 14:37:42 +0000

    Added a function that counts word

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+/**
+*_strlen_recursion - count number of letters
+*@s: a pointer variable
+*/
+int _strlen_recursion(char *s)
+{
+
+int str_len=0;
+
+if (*s != '\0')
+{
+count = count +1;
+
+_stren_recursion(s + 1);
+}
+
+return (str_len);
+}
