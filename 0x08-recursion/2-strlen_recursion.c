8bdacd428708def1686068102b723e5e27c8e646
dimka90 <dimkayilrit@gmail.com>
2023-04-05 20:34:48 +0000

    Added a function that counts returns a string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,19 @@
 /**
 *_strlen_recursion - count number of letters
 *@s: a pointer variable
+*Return: 0 for null character and a recursive call to itself plus 1
 */
 int _strlen_recursion(char *s)
 {
 
-int str_len=0;
-
-if (*s != '\0')
+if (*s == '\0')
 {
-count = count +1;
-
-_stren_recursion(s + 1);
+return  (0);
 }
+else
+{
 
-return (str_len);
+
+return (1 + _strlen_recursion(s + 1));
+}
 }
