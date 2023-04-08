4414a767f676af383ae65f87dba56c1efb1f9157
dimka90 <dimkayilrit@gmail.com>
2023-04-08 15:27:05 +0000

    A program that prints the name of a file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point , it also print the name of the
+*(file enterd on the command line)
+*@count: Keeps count of the command enterd on the shell
+*@cmd_line_command: A pointer variable that stores
+*( the command entered as string)
+*Return: Always 0(Succes)
+*/
+int main(int  count, char *cmd_line_command[])
+{
+
+printf("%s", cmd_line_command[0]);
+
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,10 @@
+Paasing command line argument to the main function in c
+int main(int argc, char *agrv[])
+{
+
+
+return 0;
+}
+
+argc(argumnt count)- Is a integer variable that keeps count of commands entered on the command line.
+argv[argument vector] - Is a pointer variable that stored each command as string in an array
