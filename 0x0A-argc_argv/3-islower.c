5a89010d9dba75c9642293fe8e477b1891139ba9
dimka90 <dimkayilrit@gmail.com>
2023-04-10 21:17:02 +0000

    Added a function  that adds positive numbers.

--- 0x0A-argc_argv/3-islower.c
+++
@@ -1,9 +0,0 @@
-/**
- * _islower - a function checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
- */
-int _islower(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}
