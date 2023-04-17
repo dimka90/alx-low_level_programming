3edddbec8672189fc21df1220dd6f3aacf4fc44d
dimka90 <dimkayilrit@gmail.com>
2023-04-17 15:36:55 +0000

    fix some logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,21 +13,19 @@ if (d == NULL)
 {
 return;
 }
-else if ((*d).name == NULL)
+if ((*d).name == NULL)
 {
 
 (*d).name = "(nil)";
 }
-else if ((*d).owner == NULL)
+if ((*d).owner == NULL)
 {
 (*d).owner = "(nil)";
 }
-else
-{
+
 printf("Name: %s\n", (*d).name);
 printf("Age: %f\n", (*d).age);
 printf("Owner: %s\n", (*d).owner);
 
-}
 
 }
