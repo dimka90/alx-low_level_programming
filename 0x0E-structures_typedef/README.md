b8a6cb0f5a9993b594320bcb74dd86470256b949
dimka90 <dimkayilrit@gmail.com>
2023-04-17 14:40:16 +0000

    created a new structure that has the dog name, age and owner name

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E. C - Structures, typedef
+Structures (also called structs) are a way to group several related variables into one place. Each variable in the structure is known as a member of the structure.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - it contains a dog information
+*@name: name of dog
+*@age: age of dog
+*@owner: name of who owns the dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+};
+
+#endif
