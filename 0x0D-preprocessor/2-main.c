fc23de607f34c1794a3bbf174ee47109693c1ff9
dimka90 <dimkayilrit@gmail.com>
2023-04-18 01:57:36 +0000

    Added  a function that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point and it also print the name a current working file
+*Return: Always 0(Success)
+*/
+int main(void)
+{
+
+printf("%s\n", __FILE__);
+return (0);
+}
