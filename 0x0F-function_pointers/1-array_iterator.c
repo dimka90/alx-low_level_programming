c0b3172981dc488872c852e2ed7a37de4498d238
dimka90 <dimkayilrit@gmail.com>
2023-04-19 15:38:00 +0000

    fix some bug

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,6 +9,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t counter;
 
+if (array == NULL || action == NULL)
+{
+return;
+}
 
 for (counter = 0; counter < size; counter++)
 {
