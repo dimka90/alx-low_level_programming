1496788c9845c241cd4266a536b7bc307e46797c
dimka90 <dimkayilrit@gmail.com>
2023-04-19 15:39:29 +0000

    added some coditionalsxy

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,15 +9,16 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t counter;
 
-if (array == NULL || action == NULL)
+if (array != NULL || action != NULL)
 {
-return;
-}
-
 for (counter = 0; counter < size; counter++)
 {
 action(array[counter]);
 
 }
-
+}
+else
+{
+return;
+}
 }
