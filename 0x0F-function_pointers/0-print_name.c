b05d9c76f987d1191411777840df22fcaf237e1c
dimka90 <dimkayilrit@gmail.com>
2023-04-19 15:00:27 +0000

    added a and operator

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,18 +9,19 @@
 void print_name(char *name, void (*f)(char *))
 {
 /**checking for NULL character in both name and function pointer */
-if (name == NULL || f == NULL)
+if (name == NULL && f == NULL)
 {
 return;
 }
-
+else
+{
 /* calling the print_name_as_is function thorough a function pointer */
 
-f(name);
+(*f(name));
 
 
 
-
+}
 
 
 
