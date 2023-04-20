c0083d3a472a2af66152803c9c954a7e880c0272
dimka90 <dimkayilrit@gmail.com>
2023-04-20 03:19:13 +0000

    fix some bug

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,14 +2,8 @@
 #define FUNCTION_POINTER_H
 
 #include <stddef.h>
-#include <stdlib.h>
-#include "function_pointers.h"
-
-void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-
-
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
 #endif
