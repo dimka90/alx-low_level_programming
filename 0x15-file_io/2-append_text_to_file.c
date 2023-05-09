9deabaf4e212a960c12a6c042dcb7265fc72e3fc
dimka90 <dimkayilrit@gmail.com>
2023-05-09 02:14:10 +0000

    fix some logic

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,15 +10,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+int o;
+int  w;
+int len = 0;
 
 if (filename == NULL)
 return (-1);
 
 if (text_content != NULL)
 {
-for (len = 0; text_content[len];)
+while(text_content)
+{
 len++;
+}
 }
 
 o = open(filename, O_WRONLY | O_APPEND);
