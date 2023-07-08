36b9006845c8edac719cf0ffc5538901cd5584c4
dimka90 <dimkayilrit@gmail.com>
2023-07-08 02:18:19 +0000

    Created a function that convert a binary to an interger

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*binary_to_uint - a function that convert an interger to binary
+*@b:a pointer vairable that points to an array of binary strings
+*Return:a converted interger
+*/
+unsigned int binary_to_uint(const char *b)
+{
+int counter;
+int binary = 2;
+unsigned int unsigned_int = 0;
+
+if (b == NULL)
+{
+return (0);
+}
+for (counter = 0; *(b + counter); counter++)
+{
+if (*(b + i) < '0' || (*(b + counter)) > '1')
+{
+return (0);
+}
+unsigned_int = binary * usigned_int + (*(b + counter) - '0');
+}
+return (unsigned_int);
+}
