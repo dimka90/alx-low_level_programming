7c9d9e51b68668520b1a533269001ed541f053fd
dimka90 <dimkayilrit@gmail.com>
2023-07-10 16:00:50 +0000

    "Coded a function that reads from a file and displays it the monitor(STDOUT)

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+*read_textfile - a function that reads text and write it out to the STDOUT"
+*@filename:The name of the file to read from
+*@letters:size of file to read
+*Return:the number of byte read
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char Buffer[1024];
+ssize_t inhandler;
+int bytes;
+if (filename == NULL)
+{
+
+return (0);
+}
+
+inhandler = open(filename, O_RDONLY);
+
+if (inhandler == -1)
+{
+return (0);
+}
+while (1)
+{
+bytes = read(inhandler, Buffer, letters);
+
+if (bytes > 0)
+{
+write(STDOUT_FILENO, Buffer, bytes);
+}
+else
+{
+
+break;
+}
+}
+
+return 1;
+}
