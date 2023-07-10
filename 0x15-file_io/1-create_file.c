85889903341e3d4b2ebf5dd573ba69467c4a4f94
dimka90 <dimkayilrit@gmail.com>
2023-07-10 18:39:00 +0000

    added a function that can create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include  "main.h"
+/**
+*create_file- a function that create a file
+*@filename: a pointer vairable for the name of the file
+*@text_content: a pointer that contains the word to be written to the file
+*Return: 1 on succes and -1 on error
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+int write_byte;
+int  file_descriptor;
+int str_len;
+
+
+
+if (filename == NULL)
+
+{
+return (-1);
+}
+if (text_content == NULL)
+{
+
+file_descriptor = open(filename, O_CREAT | O_RDWR, 0600);
+close(file_descriptor);
+}
+
+for (str_len = 0; text_content[str_len];)
+{
+str_len++;
+}
+file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+write_byte = write(file_descriptor, text_content, str_len);
+if (file_descriptor == -1 || write_byte == -1)
+{
+return (-1);
+
+}
+
+return (1);
+}
+
