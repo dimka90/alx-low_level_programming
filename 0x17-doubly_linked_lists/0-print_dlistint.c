b9f90b26874ef78e2e25b9dcfdba27b35217465d
dimka90 <dimkayilrit@gmail.com>
2023-08-17 12:08:41 +0000

    fix some bug on task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,10 +10,6 @@ size_t print_dlistint(const dlistint_t *h)
 
 const dlistint_t *tempt = h;
 size_t count = 0;
-if (h == NULL)
-{
-return (NULL);
-}
 while (tempt != NULL)
 {
 printf("%d\n", tempt->n);
