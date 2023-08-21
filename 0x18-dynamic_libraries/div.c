8fd3e16e137cf06d1dfadc4bf556a4637b3a8c1e
dimka90 <dimkayilrit@gmail.com>
2023-08-21 20:33:13 +0000

    Calling C functions python file

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,11 @@
+/**
+ * div - Divides two integers
+ * @a: First integer (dividend)
+ * @b: Second integer (divisor)
+ *
+ * Return: Result of division
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,11 @@
+/**
+ * mod - Calculates the remainder of integer division
+ * @a: First integer (dividend)
+ * @b: Second integer (divisor)
+ *
+ * Return: Remainder of division
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}
