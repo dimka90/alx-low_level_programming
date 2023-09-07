c984b15fd6080431cc6a5e118e61e93435496589
dimka90 <dimkayilrit@gmail.com>
2023-09-07 17:07:49 +0000

    fix betty error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_table_t *hash_table_create - a function that create a hash table
+ * hash_table_create - a function that create a hash table
  * @size: size of the array to create
- *
+ * Return: A pointer to a new created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
