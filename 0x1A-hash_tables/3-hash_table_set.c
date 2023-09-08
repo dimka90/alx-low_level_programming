a13b30c7a78357886c4d75faea5530bfd3a86ac0
dimka90 <dimkayilrit@gmail.com>
2023-09-08 08:20:21 +0000

    Fix some bug

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,13 +13,22 @@ size_t keylen = strlen(key) + 1;
 size_t valuelen = strlen(value) + 1;
 hash_node_t *node = malloc(sizeof(hash_node_t));
 unsigned long int hash = hash_djb2((unsigned char *) key) % ht->size;
+hash_node_t *indexnode = ht->array[hash];
 if (!ht || !key)
 	return (0);
-
-
+while (indexnode != NULL)
+{
+if (strcmp(indexnode->key, key) == 0)
+{
+free(indexnode->value);
+indexnode->value = malloc(sizeof(char) * keylen);
+strcpy(indexnode->value, value);
+return (1);
+}
+indexnode = indexnode->next;
+}
 if (node == NULL)
 	return (0);
-
 node->key = malloc(sizeof(char) * keylen);
 if (node->key == NULL)
 	return (0);
@@ -30,17 +39,7 @@ if (node->value == NULL)
 return (0);
 }
 strcpy(node->value, value);
-
-/*insertion
-
-if (ht->array[hash] == NULL)
-{
-ht->array[hash] = node;
-return (1);
-}
-*/
 node->next = ht->array[hash];
 ht->array[hash] = node;
 return (1);
-
 }
