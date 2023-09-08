4debc39c237dd9110c69274c67fa45216bd97abc
dimka90 <dimkayilrit@gmail.com>
2023-09-08 07:52:03 +0000

    added a validation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ size_t valuelen = strlen(value) + 1;
 hash_node_t *node = malloc(sizeof(hash_node_t));
 unsigned long int hash = hash_djb2((unsigned char *) key) % ht->size;
 if (!ht || !key)
-	return (0)
+	return (0);
 
 
 if (node == NULL)
