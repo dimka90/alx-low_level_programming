f9b6e2510fd1dcecd816e1d2c67128ac50ca6bf9
dimka90 <dimkayilrit@gmail.com>
2023-12-11 15:00:20 +0100

    wrote betty documentation for the function that searches the value of a specific element in an array using linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,21 +1,34 @@
 #include "search_algos.h"
+/**
+ * linear_search - Searches for a value in an array using linear search algo..
+ *
+ * @array: Pointer to the first element of the array.
+ * @size: Number of elements in the array.
+ * @value: Value to search for.
+ *
+ * Return: The first index where the value is located; or
+ *  -1 if the value is not found.
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 size_t counter;
-(void)value;
 
 for (counter = 0; counter < size; counter++)
 {
 
-	if (array == NULL) return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
 	/* printing each value in the array */
 	printf("Value checked array[%ld] = [%d]\n", counter, array[counter]);
 
 	/* checking for match in the array */
 
-	if ( array[counter] == value)
+	if (array[counter] == value)
 	{
-		return array[counter];
+		return (array[counter]);
 	}
 }
 
