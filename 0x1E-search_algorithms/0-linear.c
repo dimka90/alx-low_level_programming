da4b2624fd013d8c11bb3afd28741a7eda76a93d
dimka90 <dimkayilrit@gmail.com>
2023-12-11 14:47:39 +0100

    modify the function to search for a specify value in an array

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,9 +6,19 @@ size_t counter;
 
 for (counter = 0; counter < size; counter++)
 {
+
+	if (array == NULL) return (-1);
+	/* printing each value in the array */
 	printf("Value checked array[%ld] = [%d]\n", counter, array[counter]);
+
+	/* checking for match in the array */
+
+	if ( array[counter] == value)
+	{
+		return array[counter];
+	}
 }
 
 
-return (0);
+return (-1);
 }
